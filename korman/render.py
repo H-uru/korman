3df745f600404dffbd5fe1c0e7bc21d1252a9add
Adam Johnson <AdamJohnso@gmail.com>
2015-06-28 20:28:47 -0400

    Enable the Material Shading panel

--- korman/render.py
+++ korman/render.py
@@ -28,6 +28,7 @@ class PlasmaRenderEngine(bpy.types.RenderEngine):
 from bl_ui import properties_material
 properties_material.MATERIAL_PT_context_material.COMPAT_ENGINES.add("PLASMA_GAME")
 properties_material.MATERIAL_PT_diffuse.COMPAT_ENGINES.add("PLASMA_GAME")
+properties_material.MATERIAL_PT_shading.COMPAT_ENGINES.add("PLASMA_GAME")
 properties_material.MATERIAL_PT_specular.COMPAT_ENGINES.add("PLASMA_GAME")
 properties_material.MATERIAL_PT_options.COMPAT_ENGINES.add("PLASMA_GAME")
 properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.add("PLASMA_GAME")
