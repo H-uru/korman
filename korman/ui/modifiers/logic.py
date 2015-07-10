5d33ca7abbc8a9df8281f62674797f87ebe61d51
Adam Johnson <AdamJohnso@gmail.com>
2015-07-09 21:44:05 -0400

    Make the Advanced Logic modifier smarter
    You can now attach many node trees to this modifier and specify which
    versions of the game they should export for. Handy!

--- korman/ui/modifiers/logic.py
+++ korman/ui/modifiers/logic.py
@@ -15,8 +15,31 @@
 
 import bpy
 
+class LogicListUI(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
+        layout.prop(item, "name", emboss=False, text="", icon="NODETREE")
+
 def advanced_logic(modifier, layout, context):
-    layout.prop_search(modifier, "tree_name", bpy.data, "node_groups", icon="NODETREE")
+    row = layout.row()
+    row.template_list("LogicListUI", "logic_groups", modifier, "logic_groups", modifier, "active_group_index",
+                      rows=2, maxrows=3)
+    col = row.column(align=True)
+    op = col.operator("object.plasma_modifier_collection_add", icon="ZOOMIN", text="")
+    op.modifier = modifier.pl_id
+    op.collection = "logic_groups"
+    op.name_prefix = "Logic"
+    op.name_prop = "name"
+    op = col.operator("object.plasma_modifier_collection_remove", icon="ZOOMOUT", text="")
+    op.modifier = modifier.pl_id
+    op.collection = "logic_groups"
+    op.index = modifier.active_group_index
+
+    # Modify the loop points
+    if modifier.logic_groups:
+        logic = modifier.logic_groups[modifier.active_group_index]
+        row = layout.row()
+        row.prop_menu_enum(logic, "version")
+        row.prop_search(logic, "node_tree_name", bpy.data, "node_groups", icon="NODETREE", text="")
 
 def spawnpoint(modifier, layout, context):
     layout.label(text="Avatar faces negative Y.")
