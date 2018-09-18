H-uru/korman

commit b588c87234cee79db71e2f4b3df1f28debb18b5c

Fix issue with texcache files on stale HDDs

korman/exporter/convert.py
@@ -354,7 +354,12 @@ class Exporter:
     def texcache_path(self):
         age = bpy.context.scene.world.plasma_age
         filepath = age.texcache_path
-        if not filepath or not Path(filepath).is_file():
+        try:
+            valid_path = not filepath or not Path(filepath).is_file()
+        except OSError:
+            valid_path = False
+
+        if not valid_path:
             filepath = bpy.context.blend_data.filepath
             if not filepath:
                 filepath = self.filepath

commit d16f064043d67e030281782c68b3cb0f0bedbcfb

Fix incorrect path logic introduced in b588c87

korman/exporter/convert.py
@@ -355,7 +355,7 @@ class Exporter:
         age = bpy.context.scene.world.plasma_age
         filepath = age.texcache_path
         try:
-            valid_path = not filepath or not Path(filepath).is_file()
+            valid_path = filepath and Path(filepath).is_file()
         except OSError:
             valid_path = False
 