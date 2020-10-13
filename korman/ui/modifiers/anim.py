2f6fa7a75dfa32257ccf15f04f22ca53c6a17a94
Patrick Dulebohn <Ptrck10381@gmail.com>
2020-10-12 20:03:47 -0400

    Fix SDL state for animated lamps
    Slight indent change to enable SDL states for lamp RGB and energy animations.

--- korman/ui/modifiers/anim.py
+++ korman/ui/modifiers/anim.py
@@ -43,8 +43,8 @@ def animation(modifier, layout, context):
         col.enabled = modifier.loop and not modifier.obj_sdl_anim
         col.prop_search(modifier, "loop_start", action, "pose_markers", icon="PMARKER")
         col.prop_search(modifier, "loop_end", action, "pose_markers", icon="PMARKER")
-        layout.separator()
-        layout.prop(modifier, "obj_sdl_anim")
+    layout.separator()
+    layout.prop(modifier, "obj_sdl_anim")
         
 def animation_filter(modifier, layout, context):
     split = layout.split()
