H-uru/korman

commit 094b4fd29d3a9790a98445851fe0d8b92eca712c

Ensure all lists can be sorted.
This fixes #446 and allows all Korman lists to be sorted alphabetically
by ensuring that all collection item property groups expose a `name`
property that Blender's internal UI code can use to perform the sorting.
As part of this, I added a new UIList base class that fairly generically
handles the ad-hoc mixture of UIList subclasses. Minor behavior change -
I made the shore modifier not allow explicitly naming the shore object
references anymore. This is way out of touch with what the rest of the
plugin does. The rest of the behavior should all be unchanged.

korman/properties/modifiers/anim.py
@@ -164,6 +164,13 @@ class AnimGroupObject(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
                                  type=bpy.types.Object,
                                  poll=idprops.poll_animated_objects)
 
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.child_anim is not None:
+            return self.child_anim.name
+        return "[No Child Specified]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
     @classmethod
     def _idprop_mapping(cls):
         return {"child_anim": "object_name"}
@@ -264,12 +271,30 @@ class PlasmaAnimationGroupModifier(ActionModifier, PlasmaModifierProperties):
 
 
 class LoopMarker(bpy.types.PropertyGroup):
-    loop_name = StringProperty(name="Loop Name",
-                               description="Name of this loop")
-    loop_start = StringProperty(name="Loop Start",
-                                description="Marker name from whence the loop begins")
-    loop_end = StringProperty(name="Loop End",
-                                description="Marker name from whence the loop ends")
+    loop_name = StringProperty(options={"HIDDEN"})
+    loop_start = StringProperty(
+        name="Loop Start",
+        description="Marker name from whence the loop begins",
+        options=set()
+    )
+    loop_end = StringProperty(
+        name="Loop End",
+        description="Marker name from whence the loop ends",
+        options=set()
+    )
+
+    # Thunk for the UI - loop_name was in use from the beginning of time, unfortunately.
+    def _get_name(self) -> str:
+        return self.loop_name
+    def _set_name(self, value: str) -> None:
+        self.loop_name = value
+    name = StringProperty(
+        name="Loop Name",
+        description="Name of this loop",
+        get=_get_name,
+        set=_set_name,
+        options=set()
+    )
 
 
 class PlasmaAnimationLoopModifier(ActionModifier, PlasmaModifierProperties):

korman/ui/modifiers/anim.py
@@ -13,8 +13,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Korman.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 import bpy
 
+from ...properties.modifiers import anim as anim_mods
 from .. import ui_list
 from .. import ui_anim
 
@@ -52,11 +55,9 @@ def animation_filter(modifier, layout, context):
     col.label("Rotation:")
     col.prop(modifier, "no_rotation", text="Filter Rotation")
 
-class GroupListUI(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
-        label = item.child_anim.name if item.child_anim is not None else "[No Child Specified]"
-        icon = "ACTION" if item.child_anim is not None else "ERROR"
-        layout.label(text=label, icon=icon)
+class GroupListUI(ui_list.PlasmaUIListBase[anim_mods.AnimGroupObject], bpy.types.UIList):
+    def get_icon(self, item, icon):
+        return "ACTION" if item.child_anim is not None else "ERROR"
 
 
 def animation_group(modifier, layout, context):
@@ -70,9 +71,12 @@ def animation_group(modifier, layout, context):
         layout.prop(modifier.children[modifier.active_child_index], "child_anim", icon="ACTION")
 
 
-class LoopListUI(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
-        layout.prop(item, "loop_name", emboss=False, text="", icon="PMARKER_ACT")
+class LoopListUI(ui_list.PlasmaUIListBase[anim_mods.LoopMarker], bpy.types.UIList):
+    def get_icon(self, item, icon):
+        return "PMARKER_ACT"
+
+    def is_readonly(self, item):
+        return False
 
 
 def animation_loop(modifier, layout, context):
@@ -86,6 +90,7 @@ def animation_loop(modifier, layout, context):
     ui_list.draw_modifier_list(layout, "LoopListUI", modifier, "loops",
                                "active_loop_index", name_prefix="Loop",
                                name_prop="loop_name", rows=2, maxrows=3)
+
     # Modify the loop points
     if modifier.loops:
         loop = modifier.loops[modifier.active_loop_index]

korman/ui/modifiers/sound.py
@@ -55,13 +55,12 @@ def _draw_fade_ui(modifier, layout, label):
     layout.prop(modifier, "fade_type", text="")
     layout.prop(modifier, "length")
 
-class SoundListUI(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
-        if item.sound:
-            layout.prop(item, "name", emboss=False, icon="SOUND", text="")
-            layout.prop(item, "enabled", text="")
-        else:
-            layout.label("[Empty]")
+class SoundListUI(ui_list.PlasmaUIListBase, bpy.types.UIList):
+    def get_icon(self, item, icon):
+        return "SOUND" if item.sound is not None else "NONE"
+
+    def is_readonly(self, item) -> bool:
+        return item.sound is None
 
 
 def soundemit(modifier, layout, context):

korman/properties/modifiers/game_gui.py
@@ -489,6 +489,28 @@ class GameGuiAnimation(bpy.types.PropertyGroup):
         description="Target texture",
     )
 
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.anim_type == "OBJECT":
+            return self.target_object.name if self.target_object is not None else self.id_data.name
+        elif self.anim_type == "TEXTURE":
+            # Remember: self is not actually an instance of GameGuiAnimation.
+            name_seq = list(GameGuiAnimation._iter_target_names(self))
+            return " / ".join(name_seq)
+        else:
+            raise ValueError(self.anim_type)
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
+    def _iter_target_names(self):
+        if self.target_object is not None:
+            yield self.target_object.name
+        else:
+            yield self.id_data.name
+        if self.target_material is not None:
+            yield self.target_material.name
+        if self.target_texture is not None:
+            yield self.target_texture.name
+
 
 class GameGuiAnimationGroup(bpy.types.PropertyGroup):
     def _update_animation_name(self, context) -> None:

korman/properties/modifiers/gui.py
@@ -18,9 +18,6 @@ from __future__ import annotations
 import bpy
 from bpy.props import *
 
-from contextlib import ExitStack
-import itertools
-import math
 from pathlib import Path
 from typing import *
 
@@ -65,6 +62,13 @@ class ImageLibraryItem(bpy.types.PropertyGroup):
                                      description="Specifies whether this image will be stored during export.",
                                      default=True,
                                      options=set())
+
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.image is None:
+            return "[No Image Specified]"
+        return str(Path(self.image.name).with_suffix(".hsm"))
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
 
 
 class PlasmaImageLibraryModifier(PlasmaModifierProperties):

korman/properties/modifiers/logic.py
@@ -47,6 +47,13 @@ class PlasmaVersionedNodeTree(idprops.IDPropMixin, bpy.types.PropertyGroup):
                                 description="Node Tree to export",
                                 type=bpy.types.NodeTree)
 
+    # UI Thunk
+    def _get_name(self):
+        if self.node_tree is None:
+            return "[Empty]"
+        return self.node_tree.name
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
     @classmethod
     def _idprop_mapping(cls):
         return {"node_tree": "node_tree_name"}

korman/properties/modifiers/render.py
@@ -36,6 +36,13 @@ class PlasmaBlendOntoObject(bpy.types.PropertyGroup):
                            default=True,
                            options=set())
 
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.blend_onto is not None:
+            return self.blend_onto.name
+        return "[No Object Specified]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
 
 class PlasmaBlendMod(PlasmaModifierProperties):
     pl_id = "blend"
@@ -921,6 +928,13 @@ class VisRegion(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
                                      description="Object defining a Plasma Visibility Control",
                                      type=bpy.types.Object,
                                      poll=idprops.poll_visregion_objects)
+
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.control_region is not None:
+            return self.control_region.name
+        return "[No Object Specified]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
 
     @classmethod
     def _idprop_mapping(cls):

korman/properties/modifiers/water.py
@@ -309,11 +309,25 @@ class PlasmaWaterModifier(idprops.IDPropMixin, PlasmaModifierProperties, bpy.typ
 
 
 class PlasmaShoreObject(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
-    display_name = StringProperty(name="Display Name")
-    shore_object = PointerProperty(name="Shore Object",
-                                   description="Object that waves crash upon",
-                                   type=bpy.types.Object,
-                                   poll=idprops.poll_mesh_objects)
+    shore_object = PointerProperty(
+        name="Shore Object",
+        description="Object that waves crash upon",
+        type=bpy.types.Object,
+        poll=idprops.poll_mesh_objects
+    )
+
+    # Unused - renameable shores. Better to just show the object name.
+    display_name = StringProperty(
+        name="Display Name",
+        options={"HIDDEN"}
+    )
+
+    # UI Thunk for showing the real name.
+    def _get_name(self) -> str:
+        if self.shore_object is not None:
+            return self.shore_object.name
+        return "[Empty]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
 
     @classmethod
     def _idprop_mapping(cls):

commit 3211be087323d66fa8bc62703240277d0c0ab42c

Fix animated stereo-izing, broken in 094b4fd.
As previously noted by Sirius, when creating a temporary collection
item, simply doing an identity test later may not work due to the
collection being reallocated. In 094b4fd, the name property became a
read only thunk for UI list sorting, which caused the export to crash
when dealing with certain animated 3D sounds. To fix this,
`TemporaryCollectionItem` now requires a named string property to store
the temporary unique identifier in. You can still use `name` if it's not
a read-only thunk, but I didn't want to make that a default. It's better
for the property name to be explicit so the programmer can reason
through what's happening.

korman/helpers.py
@@ -72,15 +72,26 @@ def run_once(f):
     return wrapper
 
 @contextmanager
-def TemporaryCollectionItem(collection):
+def TemporaryCollectionItem(collection, idprop_name: str):
     item = collection.add()
-    # Blender may recreate the `item` instance as the collection grows and shrink...
-    # Assign it a unique name so we know which item to delete later on.
-    name = item.name = str(uuid4())
+    assert not item.is_property_readonly(idprop_name)
+
+    # Blender may recreate the `item` instance as the collection grows and shrink, which means we
+    # cannot do a simple identity check or use `as_pointer()`. So, we're going to assign a unique
+    # identifier to a property on the item and use that to look it up later.
+    unique_id = str(uuid4())
+    setattr(item, idprop_name, unique_id)
     try:
         yield item
     finally:
-        index = collection.find(name)
+        item_iter = (
+            idx for idx, curr_item in enumerate(collection)
+            if getattr(curr_item, idprop_name) == unique_id
+        )
+        index = next(item_iter, None)
+        if index is None:
+            raise LookupError(unique_id)
+
         collection.remove(index)
 
 class TemporaryObject:

korman/properties/modifiers/anim.py
@@ -158,11 +158,19 @@ class PlasmaAnimationModifier(ActionModifier, PlasmaModifierProperties):
 
 
 class AnimGroupObject(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
-    enabled = BoolProperty(name="Enabled", default=True)
-    child_anim = PointerProperty(name="Child Animation",
-                                 description="Object whose action is a child animation",
-                                 type=bpy.types.Object,
-                                 poll=idprops.poll_animated_objects)
+    unique_id = StringProperty(
+        options={"HIDDEN", "SKIP_SAVE"}
+    )
+    enabled = BoolProperty(
+        name="Enabled",
+        default=True
+    )
+    child_anim = PointerProperty(
+        name="Child Animation",
+        description="Object whose action is a child animation",
+        type=bpy.types.Object,
+        poll=idprops.poll_animated_objects
+    )
 
     # UI Thunk
     def _get_name(self) -> str:

korman/properties/modifiers/sound.py
@@ -630,7 +630,9 @@ class PlasmaSoundEmitter(PlasmaModifierProperties):
 
     def _add_child_animation(self, exporter, group, bo: bpy.types.Object, temporary=False):
         if temporary:
-            child = exporter.exit_stack.enter_context(TemporaryCollectionItem(group.children))
+            child = exporter.exit_stack.enter_context(
+                TemporaryCollectionItem(group.children, "unique_id")
+            )
         else:
             child = group.children.add()
         child.child_anim = bo