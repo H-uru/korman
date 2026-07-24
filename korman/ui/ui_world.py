H-uru/korman

commit 094b4fd29d3a9790a98445851fe0d8b92eca712c

Ensure all lists can be sorted.
This fixes #446 and allows all Korman lists to be sorted alphabetically
by ensuring that all collection item property groups expose a `name`
property that Blender's internal UI code can use to perform the sorting.
As part of this, I added a new UIList base class that fairly generically
handles the ad-hoc mixture of UIList subclasses. Minor behavior change -
I made the shore modifier not allow explicitly naming the shore object
references anymore. This is way out of touch with what the rest of the
plugin does. The rest of the behavior should all be unchanged.

korman/ui/ui_world.py
@@ -17,6 +17,7 @@ import bpy
 from pathlib import Path
 
 from .. import korlib
+from . import ui_list
 
 
 class AgeButtonsPanel:
@@ -172,19 +173,17 @@ class PlasmaGamePanel(AgeButtonsPanel, PlasmaGameHelper, bpy.types.Panel):
         row.menu("PlasmaGameExportMenu", icon='DOWNARROW_HLT', text="")
 
 
-class PlasmaGameListRO(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
-        layout.label(item.name, icon="BOOKMARKS")
-
-class PlasmaGameListRW(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
-        layout.prop(item, "name", text="", emboss=False, icon="BOOKMARKS")
+class PlasmaGameList(ui_list.PlasmaUIListBase, bpy.types.UIList):
+    def get_icon(self, item, icon):
+        return "BOOKMARKS"
 
 
-class PlasmaPageList(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index=0, flt_flag=0):
-        layout.prop(item, "name", text="", emboss=False, icon="BOOKMARKS")
-        layout.prop(item, "enabled", text="")
+class PlasmaPageList(ui_list.PlasmaUIListBase, bpy.types.UIList):
+    def get_icon(self, item, icon):
+        return "BOOKMARKS"
+
+    def is_readonly(self, item) -> bool:
+        return False
 
 
 class PlasmaAgePanel(AgeButtonsPanel, bpy.types.Panel):

korman/properties/modifiers/anim.py
@@ -164,6 +164,13 @@ class AnimGroupObject(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
                                  type=bpy.types.Object,
                                  poll=idprops.poll_animated_objects)
 
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.child_anim is not None:
+            return self.child_anim.name
+        return "[No Child Specified]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
     @classmethod
     def _idprop_mapping(cls):
         return {"child_anim": "object_name"}
@@ -264,12 +271,30 @@ class PlasmaAnimationGroupModifier(ActionModifier, PlasmaModifierProperties):
 
 
 class LoopMarker(bpy.types.PropertyGroup):
-    loop_name = StringProperty(name="Loop Name",
-                               description="Name of this loop")
-    loop_start = StringProperty(name="Loop Start",
-                                description="Marker name from whence the loop begins")
-    loop_end = StringProperty(name="Loop End",
-                                description="Marker name from whence the loop ends")
+    loop_name = StringProperty(options={"HIDDEN"})
+    loop_start = StringProperty(
+        name="Loop Start",
+        description="Marker name from whence the loop begins",
+        options=set()
+    )
+    loop_end = StringProperty(
+        name="Loop End",
+        description="Marker name from whence the loop ends",
+        options=set()
+    )
+
+    # Thunk for the UI - loop_name was in use from the beginning of time, unfortunately.
+    def _get_name(self) -> str:
+        return self.loop_name
+    def _set_name(self, value: str) -> None:
+        self.loop_name = value
+    name = StringProperty(
+        name="Loop Name",
+        description="Name of this loop",
+        get=_get_name,
+        set=_set_name,
+        options=set()
+    )
 
 
 class PlasmaAnimationLoopModifier(ActionModifier, PlasmaModifierProperties):

korman/properties/modifiers/game_gui.py
@@ -489,6 +489,28 @@ class GameGuiAnimation(bpy.types.PropertyGroup):
         description="Target texture",
     )
 
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.anim_type == "OBJECT":
+            return self.target_object.name if self.target_object is not None else self.id_data.name
+        elif self.anim_type == "TEXTURE":
+            # Remember: self is not actually an instance of GameGuiAnimation.
+            name_seq = list(GameGuiAnimation._iter_target_names(self))
+            return " / ".join(name_seq)
+        else:
+            raise ValueError(self.anim_type)
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
+    def _iter_target_names(self):
+        if self.target_object is not None:
+            yield self.target_object.name
+        else:
+            yield self.id_data.name
+        if self.target_material is not None:
+            yield self.target_material.name
+        if self.target_texture is not None:
+            yield self.target_texture.name
+
 
 class GameGuiAnimationGroup(bpy.types.PropertyGroup):
     def _update_animation_name(self, context) -> None:

korman/properties/modifiers/gui.py
@@ -18,9 +18,6 @@ from __future__ import annotations
 import bpy
 from bpy.props import *
 
-from contextlib import ExitStack
-import itertools
-import math
 from pathlib import Path
 from typing import *
 
@@ -65,6 +62,13 @@ class ImageLibraryItem(bpy.types.PropertyGroup):
                                      description="Specifies whether this image will be stored during export.",
                                      default=True,
                                      options=set())
+
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.image is None:
+            return "[No Image Specified]"
+        return str(Path(self.image.name).with_suffix(".hsm"))
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
 
 
 class PlasmaImageLibraryModifier(PlasmaModifierProperties):

korman/properties/modifiers/logic.py
@@ -47,6 +47,13 @@ class PlasmaVersionedNodeTree(idprops.IDPropMixin, bpy.types.PropertyGroup):
                                 description="Node Tree to export",
                                 type=bpy.types.NodeTree)
 
+    # UI Thunk
+    def _get_name(self):
+        if self.node_tree is None:
+            return "[Empty]"
+        return self.node_tree.name
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
     @classmethod
     def _idprop_mapping(cls):
         return {"node_tree": "node_tree_name"}

korman/properties/modifiers/render.py
@@ -36,6 +36,13 @@ class PlasmaBlendOntoObject(bpy.types.PropertyGroup):
                            default=True,
                            options=set())
 
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.blend_onto is not None:
+            return self.blend_onto.name
+        return "[No Object Specified]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
+
 
 class PlasmaBlendMod(PlasmaModifierProperties):
     pl_id = "blend"
@@ -921,6 +928,13 @@ class VisRegion(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
                                      description="Object defining a Plasma Visibility Control",
                                      type=bpy.types.Object,
                                      poll=idprops.poll_visregion_objects)
+
+    # UI Thunk
+    def _get_name(self) -> str:
+        if self.control_region is not None:
+            return self.control_region.name
+        return "[No Object Specified]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
 
     @classmethod
     def _idprop_mapping(cls):

korman/properties/modifiers/water.py
@@ -309,11 +309,25 @@ class PlasmaWaterModifier(idprops.IDPropMixin, PlasmaModifierProperties, bpy.typ
 
 
 class PlasmaShoreObject(idprops.IDPropObjectMixin, bpy.types.PropertyGroup):
-    display_name = StringProperty(name="Display Name")
-    shore_object = PointerProperty(name="Shore Object",
-                                   description="Object that waves crash upon",
-                                   type=bpy.types.Object,
-                                   poll=idprops.poll_mesh_objects)
+    shore_object = PointerProperty(
+        name="Shore Object",
+        description="Object that waves crash upon",
+        type=bpy.types.Object,
+        poll=idprops.poll_mesh_objects
+    )
+
+    # Unused - renameable shores. Better to just show the object name.
+    display_name = StringProperty(
+        name="Display Name",
+        options={"HIDDEN"}
+    )
+
+    # UI Thunk for showing the real name.
+    def _get_name(self) -> str:
+        if self.shore_object is not None:
+            return self.shore_object.name
+        return "[Empty]"
+    name = StringProperty(get=_get_name, options={"HIDDEN"})
 
     @classmethod
     def _idprop_mapping(cls):

korman/properties/prop_anim.py
@@ -46,6 +46,17 @@ class PlasmaAnimation(bpy.types.PropertyGroup):
                 "set": _set_animation_name,
             },
         },
+        # This is a thunk for the UIList, which expects the name property to be exactly `name`,
+        # not `animation_name`. D'oh!
+        "name": {
+            "type": StringProperty,
+            "property": {
+                "name": "Animation Name",
+                "description": "Name of this (sub-)animation",
+                "get": _get_animation_name,
+                "set": _set_animation_name,
+            },
+        },
         "start": {
             "type": IntProperty,
             "property": {

commit f7ed059018d066aa12990c289ffb3e5282911370

Fix game list in addon prefs, broken in 094b4fd.

korman/ui/ui_world.py
@@ -135,7 +135,7 @@ class PlasmaGamePanel(AgeButtonsPanel, PlasmaGameHelper, bpy.types.Panel):
 
         row = layout.row()
         # Remember: game storage moved to addon preferences!
-        row.template_list("PlasmaGameList", "games", prefs, "games", games,
+        row.template_list("PlasmaGameListRO", "games", prefs, "games", games,
                           "active_game_index", rows=2)
         row.operator("ui.korman_open_prefs", icon="PREFERENCES", text="")
 
@@ -173,9 +173,17 @@ class PlasmaGamePanel(AgeButtonsPanel, PlasmaGameHelper, bpy.types.Panel):
         row.menu("PlasmaGameExportMenu", icon='DOWNARROW_HLT', text="")
 
 
-class PlasmaGameList(ui_list.PlasmaUIListBase, bpy.types.UIList):
+class PlasmaGameListRO(ui_list.PlasmaUIListBase, bpy.types.UIList):
     def get_icon(self, item, icon):
         return "BOOKMARKS"
+
+
+class PlasmaGameListRW(ui_list.PlasmaUIListBase, bpy.types.UIList):
+    def get_icon(self, item, icon):
+        return "BOOKMARKS"
+
+    def is_readonly(self, item):
+        return False
 
 
 class PlasmaPageList(ui_list.PlasmaUIListBase, bpy.types.UIList):